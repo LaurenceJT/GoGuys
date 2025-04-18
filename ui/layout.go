8a084e2d590358fe93822b36b0d644232240d36f
Jvargas40 <ejvargas1019@gmail.com>
2025-04-18 19:51:10 -0400

    Update layout.go

--- ui/layout.go
+++ ui/layout.go
@@ -1,1 +1,21 @@
+package ui
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+)
+
+// MakeMainLayout builds the overall screen layout
+func MakeMainLayout(controls fyne.CanvasObject) fyne.CanvasObject {
+	// Placeholder: later we’ll add track info & playlist sections
+	mainContent := container.NewVBox(
+		controls,
+	)
+
+	return container.NewBorder(nil, nil, nil, nil, mainContent)
+}
+
+// DefaultWindowSize returns default app window size
+func DefaultWindowSize() fyne.Size {
+	return fyne.NewSize(800, 500)
+}
