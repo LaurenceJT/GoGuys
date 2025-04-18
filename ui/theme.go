61674df01b3695761535e6efc6ba647443e4a7fb
Jvargas40 <ejvargas1019@gmail.com>
2025-04-18 19:50:07 -0400

    Update theme.go

--- ui/theme.go
+++ ui/theme.go
@@ -1,1 +1,25 @@
 
+package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+// MakeThemeToggle returns a toggle button to switch between light and dark themes
+func MakeThemeToggle(app fyne.App) fyne.CanvasObject {
+	isDark := true // default is dark
+
+	toggle := widget.NewButton("Switch Theme", func() {
+		if isDark {
+			app.Settings().SetTheme(theme.LightTheme())
+		} else {
+			app.Settings().SetTheme(theme.DarkTheme())
+		}
+		isDark = !isDark
+	})
+
+	return container.NewVBox(toggle)
+}
